2bb3b32b3706f1129cc8f3dece853c32a9d74fbc
creandivity <creandivity@gmail.com>
2023-10-30 16:45:27 +0700

    Model

---
+++ app/Models/ManagementAccess/RoleUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\ManagementAccess;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RoleUser extends Model
+{
+    //use HasFatory;
+    use SoftDeletes;
+
+    public $table = 'role_user';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'role_id',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
---
+++ app/Models/Operational/Transaction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Operational;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    //use HasFatory;
+    use SoftDeletes;
+
+    public $table = 'transaction';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'appointment_id',
+        'fee_doctor',
+        'fee_specialist',
+        'fee_hospital',
+        'subtotal',
+        'vat',
+        'total',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
